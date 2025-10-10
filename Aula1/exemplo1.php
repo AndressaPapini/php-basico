bb9c66f7f41a4e2f395d20d1cb3d738416215ce2
Andressa Papini <andressa.papini@aluno.senai.br>
2025-10-10 16:35:40 -0300

    Exemplos de php

---
+++ Aula1/exemplo1.php
@@ -0,0 +1,13 @@
+<?php
+    //Geração de texto (string)
+    echo "Chama Sesi-Senai!";
+
+    /* 
+    Geração de texto estruturado com tags e atributos 
+    */
+    // Aspas simples dentro das aspas dupla
+   echo "<h2>Gerando <span class='destaque'>HTML</span> através do PHP.<h2>";
+
+    // Aspas simples dentro das aspas dupla
+   echo "<h2>Gerando <span class=\"destaque\">HTML</span> através do PHP.<h2>";
+?> 
---
+++ Aula1/exemplo3.php
@@ -0,0 +1,10 @@
+<?php
+echo                                   "testando espaço em branco <br>" ;
+echo "testando espaço <br>"     ;
+
+// O caracter &nbsp adiciona espaço em branco dentro da string 
+echo "&#160; &nbsp;        Testando     espaço em   branco <br>";
+echo 
+"Quebra de linha"
+; 
+?>
