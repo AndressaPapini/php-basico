8a6fb9469a36afd784357fb78e0ba5b978e4cef0
Andressa Papini <andressa.papini@aluno.senai.br>
2025-10-30 11:07:49 -0300

    Até exemplo14

--- 14_upload.php
+++ 14_upload.php
@@ -28,7 +28,7 @@
       // Move o arquivo enviado para o diretório de destino
       if (move_uploaded_file($_FILES['imagem']['tmp_name'], $caminho_completo)) {
           echo "<p>Upload realizado com sucesso!</p>";
-          echo "<img src='$caminho_completo' alt='Imagem enviada' style='max-width: 300px;'>";
+          echo "<img src='$caminho_completo' alt='Imagem enviada' style='max-width: 150px;'>";
       } else {
          echo "<p>Erro ao fazer upload do arquivo.</p>";
      }
